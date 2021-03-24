f88873a777e7030e1d7b773948704c8bfe424ec6
kmace999 <77734772+kmace999@users.noreply.github.com>
2021-03-24 17:13:55 -0400

    added shopping test (#2)

--- test/shopping_test.py
+++ test/shopping_test.py
@@ -1,4 +1,10 @@
 
 # TODO: import some code
+#using code from class
+from app.shopping import format_usd
 
 # TODO: test the code
+
+def test_format_usd():
+    result = format_usd(1999.99)
+    assert result == "$1,999.99"

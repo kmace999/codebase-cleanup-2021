a55fd0d1ed83684a9917260ccb47cccc24563c2c
kmace999 <kmace999@gmail.com>
2021-04-04 18:00:27 -0400

    format test

--- test/shopping_test.py
+++ test/shopping_test.py
@@ -6,5 +6,5 @@ from app.shopping import format_usd
 # TODO: test the code
 
 def test_format_usd():
-    result = format_usd(1999.99)
-    assert result == "$1,999.99"
+    #result = format_usd(1999.99)
+    assert format_usd(1999.99) == "$1,999.99"
